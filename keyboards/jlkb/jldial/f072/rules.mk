40d0d3fc7a4e48eadc9ec6ecb0406481650c5d0b
U-bgs\Administrator <532192607@qq.com>
2022-10-27 11:54:41 +0800

    更新

--- keyboards/jlkb/jldial/f072/rules.mk
+++ keyboards/jlkb/jldial/f072/rules.mk
@@ -1,32 +1,32 @@
 # Build Options
 #   change yes to no to disable
 #
-# MCU name
+# MCU 型号
 MCU = STM32F072
 
-# Bootloader selection
+# 引导程序 选择
 BOOTLOADER = stm32-dfu
 
-# Disable unsupported hardware
+# 启用/禁用不支持的硬件
 AUDIO_SUPPORTED = no
 BACKLIGHT_SUPPORTED = no
-BOOTMAGIC_ENABLE = yes     # Enable Bootmagic Lite
+BOOTMAGIC_ENABLE = yes     # 启用 Bootmagic Lite
 KEYBOARD_SHARED_EP = yes    # Free up some extra endpoints - needed if console+mouse+extra
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = yes           # USB Nkey Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-ENCODER_ENABLE = yes        # Enable rotary encoder support
-AUDIO_ENABLE = no           # Audio output
+MOUSEKEY_ENABLE = yes       # 鼠标键
+EXTRAKEY_ENABLE = yes       # 音频控制和系统控制
+CONSOLE_ENABLE = no         # 调试控制台
+COMMAND_ENABLE = no         # 调试和配置命令
+# 不要启用 SLEEP_LED_ENABLE。它使用与 BACKLIGHT_ENABLE 相同的计时器
+SLEEP_LED_ENABLE = no       # USB 挂起期间呼吸睡眠 LED
+# 如果这不起作用，请参见此处: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = yes           # USB Nkey 翻转
+BACKLIGHT_ENABLE = no       # 启用键盘背光功能
+ENCODER_ENABLE = yes        # 启用旋转编码器支持t
+AUDIO_ENABLE = no           # 音频输出
 RGBLIGHT_ENABLE ?= yes
 SPLIT_KEYBOARD = no
 SERIAL_DRIVER = usart
 
 MIDI_ENABLE = yes
 OLED_ENABLE = yes
-OLED_DRIVER = SSD1306    # Enable the OLED Driver
+OLED_DRIVER = SSD1306    # 启用OLED屏幕
