14033f1cf3d46e98aa4332af506f783cf86d85df
U-SJ\Administrator <532192607@qq.com>
2023-01-29 19:17:50 +0800

    更新

---
+++ keyboards/jlkb/jlk24e1/v1/config.h
@@ -0,0 +1,78 @@
+#pragma once
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x5005
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    JL-K26E1
+#define PRODUCT         JL-K26E1
+
+
+/* 层数 */
+#  define DYNAMIC_KEYMAP_LAYER_COUNT 16
+
+/* key matrix size */
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 6
+ 
+ /* 层指示灯 
+#define RGBLIGHT_LAYERS
+#define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
+#define RGBLIGHT_LAYER_BLINK   */
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS {  B13, B15, A10, B3, B5, B6}
+#define MATRIX_COL_PINS {  B12, B14, A8, A15, B4, A3}
+
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* 编码器 */
+#define ENCODERS_PAD_A { B9 }
+#define ENCODERS_PAD_B { B8 }
+#define ENCODER_RESOLUTION 2
+#define ENCODER_PAD_A_KEY_POS {4, 5}
+#define ENCODER_PAD_B_KEY_POS {2, 5}
+
+/* 编码器01 */
+#define ENCODER_DEFAULT_POS 0x3
+
+/* 指示灯 */
+/* #define LED_CAPS_LOCK_PIN B10
+#define LED_NUM_LOCK_PIN C13
+#define LED_SCROLL_LOCK_PIN B1
+#define LED_PIN_ON_STATE 0  指示灯 LED“亮”时指示灯引脚的状态 -1高电平，0低电平*/
+
+/* number of backlight levels */
+
+#ifdef BACKLIGHT_PIN
+#define BACKLIGHT_LEVELS 3
+#endif
+
+/* Set 0 if debouncing isn't needed */
+/*#define DEBOUNCING_DELAY 5 */
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* key combination for command */
+#define IS_COMMAND() ( \
+    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
+)
+
+/* prevent stuck modifiers */
+/*#define PREVENT_STUCK_MODIFIERS */
+
+#define RGB_DI_PIN B7
+#ifdef RGB_DI_PIN
+#define RGBLED_NUM 25
+#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+#endif
