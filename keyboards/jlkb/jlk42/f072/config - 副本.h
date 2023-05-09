25fbedf5bdae5af400f71a398391aa813149f878
U-bgs\Administrator <532192607@qq.com>
2023-05-09 18:00:48 +0800

    更新

--- keyboards/jlkb/jlk42/f072/config - 副本.h
+++ keyboards/jlkb/jlk42/f072/config - 副本.h
@@ -5,8 +5,8 @@
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6004
 #define DEVICE_VER      0x0002
-#define MANUFACTURER    JL\u7684
-#define PRODUCT         \u50bb\u5c4c\u952e\u76d8
+#define MANUFACTURER    JL_K26e6
+#define PRODUCT         JL_K26e6
 
 /* 层数 */
 #define DYNAMIC_KEYMAP_LAYER_COUNT 16
@@ -21,19 +21,19 @@
 #define RGBLIGHT_LAYER_BLINK */
 
 /* key matrix pins */
-#define MATRIX_ROW_PINS {  B13,B15,A10,B4,B5,B8,A15,A13}
-#define MATRIX_COL_PINS {  B12,B14,A8,B3,A4,A1,A14,B11}
+#define MATRIX_ROW_PINS {  B13,B15,A10,B4,B0,B8,A15,A13}
+#define MATRIX_COL_PINS {  B12,B14,A8,B3,B5,A5,A14,B11}
 #define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
 /* 编码器 */
-#define ENCODERS_PAD_A { A6, A3 , A0, C14, B10, B0 }
-#define ENCODERS_PAD_B { A5 , A2, C15, C13, B1, A7 }
-#define ENCODER_RESOLUTIONS {2, 2, 2, 2, 4, 4}
-#define ENCODER_PAD_A_KEY_POS {6, 0}, {6, 2}, {6, 4}, {1, 7}, {3, 7}, {5, 7}
-#define ENCODER_PAD_B_KEY_POS {6, 1}, {6, 3}, {6, 5}, {2, 7}, {4, 7}, {6, 7}
+#define ENCODERS_PAD_A { B10, B10 , A6, A4, A2, A0, C14 }
+#define ENCODERS_PAD_B { B1 , A7, A7, A3, A1, C15, C13 }
+#define ENCODER_RESOLUTIONS {4, 4, 4, 4, 4, 4, 2}
+#define ENCODER_PAD_A_KEY_POS {6, 0}, {6, 2}, {6, 4}, {1, 7}, {3, 7}, {5, 7}, {1, 4}
+#define ENCODER_PAD_B_KEY_POS {6, 1}, {6, 3}, {6, 5}, {2, 7}, {4, 7}, {6, 7}, {2, 4}
 /* 编码器01 */
 #define ENCODER_DEFAULT_POS 0x3
 
@@ -70,7 +70,7 @@
 #define RGB_DI_PIN B9
 #ifdef RGB_DI_PIN 
 #define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 28
+#define RGBLED_NUM 5
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
