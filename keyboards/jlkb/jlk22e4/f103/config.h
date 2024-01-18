38846fac2d7949740df0b607bedce2596952f510
U-bgs\Administrator <532192607@qq.com>
2024-01-18 13:02:55 +0800

    更新

---
+++ keyboards/jlkb/jlk22e4/f103/config.h
@@ -0,0 +1,71 @@
+/* Copyright 2021 ZhaQian
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+/* 映射按键*/
+#ifdef JOYSTICK_TRIGGER_ENABLE
+#define ADC_RESOLUTION ADC_CFGR1_RES_10BIT
+#define JOYSTICK_ADC_RESOLUTION 10
+#define JOYSTICK_USE_LPF
+#define JOYSTICK_LPF_PROPORTION (0.2)
+#define JOYSTICK_AXES_X_PIN { A1 }
+#define JOYSTICK_AXES_Y_PIN { A2 }
+#define JOYSTICK_AXES_PX_KEY_POS {5, 3}
+#define JOYSTICK_AXES_NX_KEY_POS {4, 3}
+#define JOYSTICK_AXES_PY_KEY_POS {5, 4}
+#define JOYSTICK_AXES_NY_KEY_POS {4, 4}
+#endif
+
+ /* 层指示灯 */
+#define RGBLIGHT_LAYERS
+#define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
+#define RGBLIGHT_LAYER_BLINK   
+#define RGBLIGHT_MAX_LAYERS 16
+
+
+/* 层数 */
+#  define DYNAMIC_KEYMAP_LAYER_COUNT 16
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 7
+
+/* 编码器01 */
+#define ENCODER_DEFAULT_POS 0x3
+
+/* 指示灯 */
+/* #define LED_CAPS_LOCK_PIN B10
+#define LED_NUM_LOCK_PIN C13
+#define LED_SCROLL_LOCK_PIN B1
+#define LED_PIN_ON_STATE 0  指示灯 LED“亮”时指示灯引脚的状态 -1高电平，0低电平*/
+
+/* Set 0 if debouncing isn't needed */
+/* #define DEBOUNCING_DELAY 5 */
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* key combination for command */
+#define IS_COMMAND() ( \
+    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
+)
+
+/* prevent stuck modifiers */
+/* #define PREVENT_STUCK_MODIFIERS */
---
+++ keyboards/jlkb/jlk22e4/f103/keymaps/vial/keymap.c
@@ -0,0 +1,127 @@
+#include "f103.h"
+enum keycodes {
+  LAYERS_DOWN = SAFE_RANGE,
+  LAYERS_UP,
+  jltb,
+  jldu,
+  jldu1
+};
+
+// 1st layer on the cycle
+#define LAYER_CYCLE_START 0
+// Last layer on the cycle
+#define LAYER_CYCLE_END   16
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+	LAYOUT(
+		KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, 
+		KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, 
+		KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, 
+		KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, 
+		KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC),
+
+	LAYOUT(
+		KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, 
+		KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, 
+		KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, 
+		KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, 
+		KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_SPC)
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] =   { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), ENCODER_CCW_CW(KC_VOLD, KC_VOLU),    ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+    [1] =   { ENCODER_CCW_CW(RGB_HUD, RGB_HUI),           ENCODER_CCW_CW(RGB_SAD, RGB_SAI),    ENCODER_CCW_CW(RGB_SAD, RGB_SAI)  },
+
+    //                  旋钮 1                                     旋钮 2                                      旋钮 3               
+};
+#endif
+
+// Add the behaviour of this new keycode
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // 下一层
+  switch (keycode) {
+    case LAYERS_DOWN:
+      // Our logic will happen on presses, nothing is done on releases
+      if (!record->event.pressed) { 
+        // We've already handled the keycode (doing nothing), let QMK know so no further code is run unnecessarily
+        return false;
+      }
+
+      uint8_t current_layer = get_highest_layer(layer_state);
+
+      // Check if we are within the range, if not quit
+      if (current_layer > LAYER_CYCLE_END || current_layer < LAYER_CYCLE_START) {
+        return false;
+      }
+
+      uint8_t next_layer = current_layer + 1;
+      if (next_layer > LAYER_CYCLE_END) {
+          next_layer = LAYER_CYCLE_START;
+      }
+      layer_move(next_layer);
+      return false;
+
+    // Process other keycodes normally
+
+     return true;
+     break;
+   // 上一层  
+      case LAYERS_UP:
+      // Our logic will happen on presses, nothing is done on releases
+      if (!record->event.pressed) { 
+        // We've already handled the keycode (doing nothing), let QMK know so no further code is run unnecessarily
+        return false;
+      }
+
+      uint8_t current1_layer = get_highest_layer(layer_state);
+
+      // Check if we are within the range, if not quit
+      if (current1_layer > LAYER_CYCLE_END || current1_layer < LAYER_CYCLE_START) {
+        return false;
+      }
+
+      uint8_t previous_layer = current1_layer - 1;
+      if (previous_layer > LAYER_CYCLE_END) {
+          previous_layer = LAYER_CYCLE_START;
+      }
+      layer_move(previous_layer);
+      return false;
+
+    // Process other keycodes normally
+     default:
+     return true;
+     break;
+     
+ case jltb:
+        if (record->event.pressed) {
+            SEND_STRING(SS_DOWN(X_LGUI) SS_TAP(X_R) SS_UP(X_LGUI) SS_DELAY(100) "https://jlkb.taobao.com" SS_TAP(X_ENTER) SS_TAP(X_ENTER));
+        }
+        
+    return true;
+    break;
+    
+ case jldu:
+        if (record->event.pressed) {
+            layer_on(1);
+            return true;
+            layer_off(1);
+        }else {
+            tap_code(KC_K); 
+      }
+    return false;
+    break;
+    
+    case jldu1:
+        if (record->event.pressed) {
+            tap_code(KC_L);
+        }else {
+            tap_code(KC_K); 
+      }
+    return false;
+    break;
+    
+  }
+}
+
