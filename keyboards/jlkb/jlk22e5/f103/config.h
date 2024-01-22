05d1b7d39f3116a5eac787fac7ab30b86b976405
U-LHF\Administrator <532192607@qq.com>
2024-01-22 23:24:23 +0800

    更新

--- keyboards/jlkb/jlk22e5/f103/config.h
+++ keyboards/jlkb/jlk22e5/f103/config.h
@@ -1,22 +1,20 @@
-/* Copyright 2021 ZhaQian
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #pragma once
+#include "config_common.h"
 
-/* 映射按键*/
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x6004
+#define DEVICE_VER      0x0002
+#define MANUFACTURER    JL\u7684
+#define PRODUCT         \u5927\u5e08\u4e13\u4e1a\u952e\u76d8
+
+/*
+#define ANALOG_JOYSTICK_X_AXIS_PIN A2   // 上拉 X轴右移
+#define ANALOG_JOYSTICK_Y_AXIS_PIN A1   // 上拉 Y轴下移 
+#define ANALOG_JOYSTICK_CLICK_PIN B1
+#define POINTING_DEVICE_ROTATION_180   // 将 X 和 Y 数据旋转 180 度 
+*/
+
 #ifdef JOYSTICK_TRIGGER_ENABLE
 #define ADC_RESOLUTION ADC_CFGR1_RES_10BIT
 #define JOYSTICK_ADC_RESOLUTION 10
@@ -24,10 +22,10 @@
 #define JOYSTICK_LPF_PROPORTION (0.2)
 #define JOYSTICK_AXES_X_PIN { A1 }
 #define JOYSTICK_AXES_Y_PIN { A2 }
-#define JOYSTICK_AXES_PX_KEY_POS {3, 4}
-#define JOYSTICK_AXES_NX_KEY_POS {4, 4}
-#define JOYSTICK_AXES_PY_KEY_POS {1, 4}
-#define JOYSTICK_AXES_NY_KEY_POS {2, 4}
+#define JOYSTICK_AXES_PX_KEY_POS {2, 4}
+#define JOYSTICK_AXES_NX_KEY_POS {3, 4}
+#define JOYSTICK_AXES_PY_KEY_POS {0, 4}
+#define JOYSTICK_AXES_NY_KEY_POS {1, 4}
 #endif
 
  /* 层指示灯 */
@@ -36,22 +34,32 @@
 #define RGBLIGHT_LAYER_BLINK   
 #define RGBLIGHT_MAX_LAYERS 16
 
-
 /* 层数 */
-#  define DYNAMIC_KEYMAP_LAYER_COUNT 16
+#define DYNAMIC_KEYMAP_LAYER_COUNT 16
 
 /* key matrix size */
 #define MATRIX_ROWS 7
 #define MATRIX_COLS 7
+ 
+ /* 层指示灯 
+#define RGBLIGHT_LAYERS
+#define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
+#define RGBLIGHT_LAYER_BLINK */
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS { C13, C14, C15, A0, A3, A14, A13 }
+#define MATRIX_COL_PINS { B9, B8, B5, B4, B3, A15, B13 }
+#define UNUSED_PINS
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
 
 /* 编码器 */
-//#define ENCODERS_PAD_A { B10, B0 , A6, C14 }
-//#define ENCODERS_PAD_B { B1, A7 , A5, C13 }
-//#define ENCODER_RESOLUTIONS { 4,4,4,4,4 }
-/* 映射矩阵 C，R*/
-#define ENCODER_PAD_A_KEY_POS {0, 5}, {2, 5}, {4, 5}, {6, 5}, {0, 6}
-#define ENCODER_PAD_B_KEY_POS {1, 5}, {3, 5}, {5, 5}, {4, 4}, {1, 6}
-
+#define ENCODERS_PAD_A { A8, A4, A6, B0, B10, A10 }
+#define ENCODERS_PAD_B { B15, A5, A7, B1, B11, B14 }
+#define ENCODER_RESOLUTIONS {4, 4, 2, 4, 4, 4 }
+#define ENCODER_PAD_A_KEY_POS {0, 5}, {2, 5}, {4, 5}, {6, 5}, {0, 6}, {2, 6}
+#define ENCODER_PAD_B_KEY_POS {1, 5}, {3, 5}, {5, 5}, {4, 4}, {1, 6}, {3, 6}
 /* 编码器01 */
 #define ENCODER_DEFAULT_POS 0x3
 
@@ -60,6 +68,12 @@
 #define LED_NUM_LOCK_PIN C13
 #define LED_SCROLL_LOCK_PIN B1
 #define LED_PIN_ON_STATE 0  指示灯 LED“亮”时指示灯引脚的状态 -1高电平，0低电平*/
+
+/* number of backlight levels */
+
+#ifdef BACKLIGHT_PIN
+#define BACKLIGHT_LEVELS 3
+#endif
 
 /* Set 0 if debouncing isn't needed */
 /* #define DEBOUNCING_DELAY 5 */
@@ -77,3 +91,13 @@
 
 /* prevent stuck modifiers */
 /* #define PREVENT_STUCK_MODIFIERS */
+
+
+#define RGB_DI_PIN B12
+#ifdef RGB_DI_PIN 
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 22
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+#endif
