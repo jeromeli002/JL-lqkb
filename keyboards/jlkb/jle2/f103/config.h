3b514f0d14c47d83dbb927ccf0bdc40b24ef4078
jeromeli002 <532192607@qq.com>
2025-01-13 14:17:48 +0800

    Update config.h

--- keyboards/jlkb/jle2/f103/config.h
+++ keyboards/jlkb/jle2/f103/config.h
@@ -31,22 +31,18 @@
 // 矩阵位置
 #define JOYSTICK_ACTUATION 256  // 触发点，即摇杆偏移多少开始触发按键 (0-511)
 #define JOYSTICK_HYSTERESIS 50  // 滞后量，防止抖动，调整此值以达到最佳效果
-#define JOYSTICK_KEY_X_PIN  A1 
-#define JOYSTICK_KEY_Y_PIN  A2 
-#define JOYSTICK_KEY_PX_POS {1, 3}
-#define JOYSTICK_KEY_NX_POS {1, 2}
-#define JOYSTICK_KEY_PY_POS {0, 2}
-#define JOYSTICK_KEY_NY_POS {0, 1}
+#define JOYSTICK_KEY_X_PIN  {A1,A6}
+#define JOYSTICK_KEY_Y_PIN  {A2,A7}
+#define JOYSTICK_KEY_PX_POS {{1, 3},{0,0}}
+#define JOYSTICK_KEY_NX_POS {{1, 2},{0,0}}
+#define JOYSTICK_KEY_PY_POS {{0, 2},{0,0}}
+#define JOYSTICK_KEY_NY_POS {{0, 1},{0,0}}
 
 // 摇杆按键数 Max 32
 #define JOYSTICK_BUTTON_COUNT 0
 // 每个摇杆轴数量 Max 6: X, Y, Z, Rx, Ry, Rz
 #define JOYSTICK_AXIS_COUNT 2 //轴数量X/Y轴
 
-/*EEPROM调整*/
-#define WEAR_LEVELING_LOGICAL_SIZE 2048
-#define WEAR_LEVELING_BACKING_SIZE (WEAR_LEVELING_LOGICAL_SIZE * 2)
-#define WS2812_BYTE_ORDER       WS2812_BYTE_ORDER_GRB
 
 /* 层数 */
 #  define DYNAMIC_KEYMAP_LAYER_COUNT 16
